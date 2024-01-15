3d3dfdb00adf875ba189721cf645d25390a670ef
Alin-Romeo Tudose <alintudose126@gmail.com>
2024-01-15 21:34:42 +0200

    SpringInitializr spring6webapp
    Signed-off-by: Alin-Romeo Tudose <alintudose126@gmail.com>

---
+++ spring6webapp/src/main/java/com/alint/spring6learning/spring6webapp/Spring6webappApplication.java
@@ -0,0 +1,11 @@
+package com.alint.spring6learning.spring6webapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring6webappApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(Spring6webappApplication.class, args);
+    }
+}
---
+++ spring6webapp/src/test/java/com/alint/spring6learning/spring6webapp/Spring6webappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alint.spring6learning.spring6webapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring6webappApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
