6b7559e1dcada58c6f689ca879be45b58f7027ea
Alin-Romeo Tudose <alintudose126@gmail.com>
2024-01-16 17:39:22 +0200

    Generated SpringInitializr empty project
    Signed-off-by: Alin-Romeo Tudose <alintudose126@gmail.com>

---
+++ Spring6DIProject/src/main/java/com/alint/spring6learning/spring6diproject/Spring6DiProjectApplication.java
@@ -0,0 +1,13 @@
+package com.alint.spring6learning.spring6diproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring6DiProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Spring6DiProjectApplication.class, args);
+    }
+
+}
